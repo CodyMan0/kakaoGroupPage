c5149e719b45054d39df1ea2e1fac1a81bd70b27
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-12-22 14:49:44 +0900

    [feat] 소셜 로그인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './page/SignIn';
+import KakaoRedirectHandler from './page/KakaoRedirectHandler';
+import Home from './page/Home';
 
 function App() {
   return (
     <BrowserRouter basename="/kakaoGroupPage">
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route
+          path="/oauth/callback/kakao"
+          element={<KakaoRedirectHandler />}
+        />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
