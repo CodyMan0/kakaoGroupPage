95c07de2d64d497187f8245fd333dc0a227f2ba2
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-12-24 14:01:33 +0900

    [fix] conflict 해결 중

--- src/api/getAuthorization.tsx
+++ src/api/getAuthorization.tsx
@@ -1,32 +1,15 @@
 import { KAKAO_TOKEN_URL, LOGIN } from '../constants/config';
 
 export const getRequest = async (code: string | null): Promise<any> => {
-<<<<<<< HEAD
-=======
-  console.log('getRequest');
->>>>>>> main
   const requestUrl = `${KAKAO_TOKEN_URL}?grant_type=authorization_code&client_id=${
     import.meta.env.VITE_CLIENT_KEY
   }&redirect_uri=${LOGIN.REDIRECT_URI}&code=${code}`;
   const requestHeaders = {
     method: 'POST',
   };
-<<<<<<< HEAD
-  const result = await fetch(requestUrl, requestHeaders);
-
-  if (result.ok === true) {
-    return result.json();
-  }
-};
-
-// export const getUserInfo = async (params: type) => {
-//   const;
-// };
-=======
   const response = await fetch(requestUrl, requestHeaders);
 
   if (response.ok === true) {
     return response.json();
   }
 };
->>>>>>> main
