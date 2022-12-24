06ab0f67a58ad0f5033bd30656af2f36100e87db
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-12-24 18:13:57 +0900

    [test] 베포 환경에서도 로그인이 되는지 테스트

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,11 @@
 export const LOGIN = {
   BASE_URL: 'https://kauth.kakao.com',
-  REDIRECT_URI: 'http://127.0.0.1:5173/kakaoGroupPage/oauth/callback/kakao',
+  REDIRECT_URI:
+    'https://codyman0.github.io/kakaoGroupPage/oauth/callback/kakao',
 };
+
+// 내 로컬 주소 http://127.0.0.1:5173
+// 베포 주소 https://codyman0.github.io
 
 export const KAKAO_AUTH_URL = `${LOGIN.BASE_URL}/oauth/authorize?client_id=${
   import.meta.env.VITE_CLIENT_KEY
